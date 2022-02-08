b859f10210907ad88f2d88acb7f82bfe19f21a5e
Ahaochan <844394093@qq.com>
2022-02-08 14:42:28 +0800

    🔊 104 深入探索BrokerOuter API是如何发送注册请求的

--- remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyRemotingClient.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyRemotingClient.java
@@ -379,6 +379,7 @@ public class NettyRemotingClient extends NettyRemotingAbstract implements Remoti
     public RemotingCommand invokeSync(String addr, final RemotingCommand request, long timeoutMillis)
         throws InterruptedException, RemotingConnectException, RemotingSendRequestException, RemotingTimeoutException {
         long beginStartTime = System.currentTimeMillis();
+        // 1. 创建Channel连接
         final Channel channel = this.getAndCreateChannel(addr);
         if (channel != null && channel.isActive()) {
             try {
@@ -387,6 +388,7 @@ public class NettyRemotingClient extends NettyRemotingAbstract implements Remoti
                 if (timeoutMillis < costTime) {
                     throw new RemotingTimeoutException("invokeSync call the addr[" + addr + "] timeout");
                 }
+                // 2. 执行注册请求
                 RemotingCommand response = this.invokeSyncImpl(channel, request, timeoutMillis - costTime);
                 doAfterRpcHooks(RemotingHelper.parseChannelRemoteAddr(channel), request, response);
                 return response;
@@ -413,11 +415,13 @@ public class NettyRemotingClient extends NettyRemotingAbstract implements Remoti
             return getAndCreateNameserverChannel();
         }
 
+        // 1. 从缓存取Channel
         ChannelWrapper cw = this.channelTables.get(addr);
         if (cw != null && cw.isOK()) {
             return cw.getChannel();
         }
 
+        // 2. 取不到就创建一个Channel
         return this.createChannel(addr);
     }
 
@@ -492,6 +496,7 @@ public class NettyRemotingClient extends NettyRemotingAbstract implements Remoti
                 }
 
                 if (createNewConnection) {
+                    // 这里的Bootstrap是Netty自己的东西了, 这里会创建一个真正的Channel连接
                     ChannelFuture channelFuture = this.bootstrap.connect(RemotingHelper.string2SocketAddress(addr));
                     log.info("createChannel: begin to connect remote host[{}] asynchronously", addr);
                     cw = new ChannelWrapper(channelFuture);
